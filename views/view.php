58157110a6cad38a9fab777ba929a8097ddd9704
TimCH <caramou62@gmail.com>
2015-03-17 14:37:06 +0100

    iteration 4

--- views/view.php
+++ views/view.php
@@ -10,12 +10,10 @@
             <h1>MicroCMS</h1>
         </header>
         <?php
-        $bdd = new PDO('mysql:host=localhost;dbname=microcms;charset=utf8', 'microcms_user', 'secret');
-        $articles = $bdd->query('select * from t_article order by art_id desc');
         foreach ($articles as $article): ?>
         <article>
-            <h2><?php echo $article['art_title'] ?></h2>
-            <p><?php echo $article['art_content'] ?></p>
+            <h2><?php echo $article->getTitle() ?></h2>
+            <p><?php echo $article->getContent() ?></p>
         </article>
         <?php endforeach ?>
         <footer class="footer">
